14063de3c0fbfe751be4a652e35ca7cb05ca40f8
ant-cave <ANTmmmmm@126.com>
2026-04-04 20:28:29 +0800

    test tokio

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -22,6 +22,16 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, get_foreground_window_info])
+        .setup(|_app| {
+            // 启动一个异步任务，每秒输出 hello
+            tauri::async_runtime::spawn(async {
+                loop {
+                    tracing::info!("hello");
+                    tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
+                }
+            });
+            Ok(())
+        })
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
